cd3e4d6e4395fc53e210a5afc852c8fde97be104
Ameya <adengane@gmai.com>
2022-07-02 16:39:49 +0530

    added navbar

--- client/app/src/Components/Deposit/Deposit.js
+++ client/app/src/Components/Deposit/Deposit.js
@@ -3,7 +3,8 @@ import { ethers } from "ethers";
 import { useSelector } from "react-redux";
 //b
 import axios from "axios";
-import Gamble from "../../contracts/Gamble.sol";
+import Gamble from "../../artifacts/contracts/Gamble.sol/Gamble.json";
+
 const Deposit = () => {
   const acc = useSelector((state) => state.db.userAcc);
   const [matic, setMatic] = useState(1);
